fc62c06ca9e81bc39282bc9f108d23061773a61a
Matthew Star <your.mattjstar@gmail.com>
2013-08-17 17:33:18 -0400

    halfway through 9

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -5,3 +5,15 @@ RSpec::Matchers.define :have_error_message do |message|
     expect(page).to have_selector('div.alert.alert-error', text: message)
   end
 end
+
+def sign_in(user)
+	visit signin_path
+	fill_in "Email",    with: user.email
+  	fill_in "Password", with: user.password
+  	click_button "Sign in"
+  	# Sign in when not using Capybara 
+  	# (the put requests in authentication_pages_spec).
+  	# You can't have cookies in your test network (53:56 of video 9) so we 
+  	# manually add them here:
+  	cookies[:remember_token] = user.remember_token
+end
