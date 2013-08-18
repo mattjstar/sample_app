273a6c39a639058bd15ded91ad0e1f52fd9b24da
Matthew Star <your.mattjstar@gmail.com>
2013-08-18 17:24:01 -0400

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
+describe "Authentication" do
   
   subject { page }
 
@@ -36,6 +36,7 @@ describe "AuthenticationPages" do
   		it { should have_link('Profile',     href: user_path(user)) }
   		it { should have_link('Sign out',    href: signout_path) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Users',       href: users_path) }
   		it { should_not have_link('Sign in', href: signin_path)}
   	 
       describe "followed by signout" do
@@ -91,6 +92,11 @@ describe "AuthenticationPages" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
       end
     end    
 
@@ -106,6 +112,18 @@ describe "AuthenticationPages" do
 
       describe "submitting a PUT request to the Users#update action" do
         before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a delete request to the Users#destroy action" do
+        before { delete user_path(user) }
         specify { response.should redirect_to(root_path) }
       end
     end
